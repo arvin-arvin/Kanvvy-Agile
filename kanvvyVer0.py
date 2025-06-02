5689172d99b4ed764b2ba271d6317d434102add3
arvin-arvin <arvinhcrisostomo@gmail.com>
2025-06-03 11:40:42 +1200

    Ver 0 files and Assets

---
+++ kanvvyVer0.py
@@ -0,0 +1,235 @@
+import tkinter as tk
+import pyglet as pyg
+import json
+root = tk.Tk()
+root.geometry("1280x720")
+root.resizable(0,0)
+root.title("Kanvvy")
+root.config(bg="#141414")
+
+dark = "#141414"
+light = "#ffffff"
+pyg.font.add_file("Agile/CalSans-Regular.ttf")
+
+with open('Agile/jsons/General.json', 'r', encoding='utf-8') as f:
+    generalData = json.load(f)
+
+
+print(generalData[0]['gif'])
+
+# SHOW THE CATEGORY BUTTONS
+def start():
+    for i in root.winfo_children():
+        i.pack_forget()
+
+    def enter(event):
+        event.widget.config(fg="#b59bf9",
+                            borderwidth=7,
+                            highlightcolor="#b59bf9",
+                            relief="ridge"
+                            )
+        
+    def leave(event):
+        event.widget.config(fg=dark,
+                            borderwidth=7,
+                            relief="raised"
+                            )
+        
+    
+    def enter2(event):
+        event.widget.config(fg="lightblue",
+                            borderwidth=7,
+                            highlightcolor="lightblue",
+                            relief="ridge"
+                            )
+        
+    def leave2(event):
+        event.widget.config(fg=dark,
+                            borderwidth=7,
+                            relief="raised"
+                            )
+
+    desc2.grid(row=0, column=0, columnspan=4, pady=20, sticky="ew")
+    desc3.grid(row=1, column=0, columnspan=4, sticky="ew")
+    
+    full_row_button = tk.Button(root, text="Random  ランダム", font=("arial", 20), borderwidth=7, relief="raised", cursor="hand2")
+    full_row_button.grid(row=2, column=0, columnspan=4, padx=5, pady=5, sticky="ew")
+    full_row_button.bind("<Enter>", enter2)
+    full_row_button.bind("<Leave>", leave2)
+
+
+    button_1 = tk.Button(root, text="普通\nGeneral", command=start_general,font=("arial", 15), borderwidth=7, relief="raised", cursor="hand2")
+    button_1.grid(row=3, column=0, padx=10, pady=5, sticky="nsew")
+    button_2 = tk.Button(root, text="数\nNumbers",font=("arial", 15), borderwidth=7, relief="raised", cursor="hand2")
+    button_2.grid(row=3, column=1, padx=10, pady=5, sticky="nsew")
+    button_3 = tk.Button(root, text="行動\nActions",font=("arial", 15), borderwidth=7, relief="raised", cursor="hand2")
+    button_3.grid(row=3, column=2, padx=10, pady=5, sticky="nsew")
+    button_4 = tk.Button(root, text="食品\nFood",font=("arial", 15), borderwidth=7, relief="raised", cursor="hand2")
+    button_4.grid(row=3, column=3, padx=10, pady=5, sticky="nsew")
+
+    button_1.bind("<Enter>", enter)
+    button_1.bind("<Leave>", leave)
+    button_2.bind("<Enter>", enter)
+    button_2.bind("<Leave>", leave) 
+    button_3.bind("<Enter>", enter)
+    button_3.bind("<Leave>", leave) 
+    button_4.bind("<Enter>", enter)
+    button_4.bind("<Leave>", leave) 
+
+ 
+    for i in range(4):
+        root.grid_rowconfigure(i, weight=1)
+    for i in range(4):
+        root.grid_columnconfigure(i, weight=1)
+
+# VALUE METHOD FOR CHANGING JSON DATA
+value = 1
+def nextPage():
+        value = value +  1
+        start_general()
+def lastPage():
+        value = value - 1
+        start_general()  
+
+
+# PICKED GENERAL START
+def start_general():
+    for i in root.winfo_children():
+        i.grid_forget()
+
+    
+
+    name = tk.Label(root, text=generalData[value]["name"],
+                 font=("Myriad Pro", 20),
+                 bg=dark,
+                 fg=light)
+
+    imgg = tk.PhotoImage(file=generalData[value]['img'])
+    img = tk.Label(root, image=imgg)
+
+    giff = tk.PhotoImage(file=generalData[value]['gif'], format="gif -index 2")
+    gif = tk.Label(root, image=giff)
+
+    read = tk.Label(root, text=generalData[value]["read"],
+                 font=("Myriad Pro", 10),
+                 bg=dark,
+                 fg=light)
+    
+    backBtn = tk.Button(root, text="BACK", command=lastPage, bg="lightblue", font=("arial", 35), borderwidth=7, relief="raised", cursor="hand2")
+
+    nextBtn = tk.Button(root, text="NEXT", command=nextPage, bg="lightblue", font=("arial", 35), borderwidth=7, relief="raised", cursor="hand2")
+
+    name.grid(row=0, column=1, padx=10, pady=5, columnspan=2, sticky="nsew")
+    img.grid(row=1, column=1, padx=10, pady=5, columnspan=2, sticky="nsew")
+    gif.grid(row=2, column=1, padx=10, pady=5, columnspan=2, sticky="nsew")
+    read.grid(row=3, column=1, padx=10, pady=5, columnspan=2, sticky="nsew")
+    backBtn.grid(row=4, column=1, padx=10, pady=5, sticky="nsew")
+    nextBtn.grid(row=4, column=2, padx=10, pady=5, sticky="nsew")
+
+    for i in range(4):
+        root.grid_rowconfigure(i, weight=1)
+    for i in range(4):
+        root.grid_columnconfigure(i, weight=1)
+
+  
+# START BUTTON INNITIALIZATION
+def show_start_button():
+
+    def enter(event):
+        event.widget.config(fg="#b59bf9",
+                            borderwidth=7,
+                            highlightcolor="#b59bf9",
+                            relief="ridge"
+                            )
+        
+    def leave(event):
+        event.widget.config(fg=dark,
+                            borderwidth=7,
+                            relief="raised"
+                            )
+        
+
+    for widget in root.winfo_children():
+        widget.grid_forget()
+
+    start_button = tk.Button(text="START",
+                    command=start,
+                    borderwidth=7,
+                    font=("arial", 20),
+                    relief="raised",
+                    cursor="hand2",
+                    fg=dark) 
+    start_button.pack(padx=20, pady=20)
+
+    start_button.bind("<Enter>", enter)
+    start_button.bind("<Leave>", leave) 
+
+
+    
+
+    
+
+    
+
+
+    
+    
+desc2 = tk.Label(text="Click on a category to start learning Kanji strokes, there will be a quick quiz afterwards!",
+                 font=("Myriad Pro", 20),
+                 bg=dark,
+                 fg=light)
+                 
+desc3 = tk.Label(text="Have fun studying! お勉強頑張ってお楽しみしよう！！",
+                 font=("Myriad Pro", 20),
+                 bg=dark,
+                 fg=light)
+
+
+
+title = tk.Label(text="Welcome to Kanvvy",
+                 font=("arial", 20),
+                 bg=dark,
+                 fg=light)
+title.pack(pady=20)
+
+descMain = tk.Label(text="This is a small language learning app for Japanese ( 日本語 ), this app will mainly train your knowledge in",
+                font=("Myriad Pro", 15),
+                justify="center",
+                bg=dark,
+                fg=light)
+descMain.pack(pady=5)
+
+descMain2 = tk.Label(text="Kanji regarding its meaning and Kanji Strokes in categories, have fun!",
+                font=("Myriad Pro", 15),
+                justify="center",
+                bg=dark,
+                fg=light)
+descMain2.pack(pady=5)
+
+ins = tk.Label(text="INSTRUCTIONS:",
+                font=("arial", 20),
+                bg=dark,
+                fg=light)
+ins.pack(pady=50)
+
+
+ins_desc = tk.Label(text="Write by yourself!",
+                font=("Myriad Pro", 20),
+                bg=dark,
+                fg=light)
+ins_desc.pack(pady=5)
+
+ins_desc = tk.Label(text="Grab a pen and paper, notebook and pencil, anything! Writing will make it easier to understand and ",
+                font=("Myriad Pro", 15),
+                bg=dark,
+                fg=light)
+ins_desc.pack(pady=5)
+ins_desc2 = tk.Label(text="memorize the Kanjis, it will also train your stability in writing and overall knowledge in stroke orders.",
+                font=("Myriad Pro", 15),
+                bg=dark,
+                fg=light)
+ins_desc2.pack(pady=5)
+
+
+show_start_button()
+root.mainloop()
